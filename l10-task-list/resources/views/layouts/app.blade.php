59dce2cb8394f8e7a1275da073266b3898a70e42
Kwadwo Sarbeng-Baafi <ksarbengbaafi@gmail.com>
2024-07-02 14:32:46 +0000

    book review project started}

--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -1,40 +1,61 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel 10 Task List App</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-        {{-- blade-formatter-disable --}}
-        <style type="text/tailwindcss">
-           .btn{
-            @apply rounded-md px-2 py-1 text-center text-slate-700  font-medium shadow-sm ring-1 ring-slate-300 hover:bg-slate-50 
+
+<head>
+    <title>Laravel 10 Task List App</title>
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn{
+@applyrounded-md px-2 py-1 text-center text-slate-700  font-medium shadow-sm ring-1 ring-slate-300 hover:bg-slate-50
            }
            .link {
-            @apply font-medium decoration-pink-500 underline text-gray-700
+@applyfont-medium decoration-pink-500 underline text-gray-700
            }
            label {
-            @apply block uppercase text-slate-700
+@applyblock uppercase text-slate-700
            }
            input,textarea {
-            @apply shadow-sm appearance-none border w-full py-2 leading-tight focus:outline-none
+@applyshadow-sm appearance-none border w-full py-2 leading-tight focus:outline-none
            }
 
            .error {
-            @apply text-red-500 text-sm
+@applytext-red-500 text-sm
            }
         </style>
-        {{-- blade-formatter-enable --}}
-       
-    </head>
-    <body class="container mx-auto mt-10 mb-10 max-w-lg">
-      
-        <h1 class="mb-4 text-2xl">@yield('title')</h1>
-        <div class="mb-10 font-bold">This is a flash message <div>
-        <!-- <div>{{ session('success')}}</div> -->
-        <!-- @if (session()->has('success'))
-            <div>{{ session('success')}}</div>
-        @endif -->
-        <div>
-            @yield('content')
-        </div>
-    </body>
+    {{-- blade-formatter-enable --}}
+
+</head>
+
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
+
+    <!-- <div>{{ session('success') }}</div> -->
+    <!-- @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+@endif-->
+
+    @if (session()->has('success'))
+    <div x-data="{flash : true }">
+        <h2 role="alert" x-show="flash"
+            class="relative mb-10 border border-green-400 bg-green-100 px-4 py-3 text-green-900 rounded text-lg">
+            <strong class="font-bold">Success!</strong>
+           <div>{{ session('success') }}</div>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
+        </h2>
+    </div>
+    @endif
+    <div>
+        @yield('content')
+    </div>
+</body>
+
 </html>
